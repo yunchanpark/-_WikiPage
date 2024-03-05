c59b611deee6205f0b1c097a3f4bfb51156bdd47
yunchanpark <yongp99@nate.com>
2024-03-06 01:26:32 +0900

    add: 조회 페이지 데이터 패칭 구현

--- src/app/wiki/list/page.tsx
+++ src/app/wiki/list/page.tsx
@@ -1,7 +1,19 @@
-export default function WikiListPage() {
+import prisma from '@/db';
+
+export default async function WikiListPage() {
+    const wikiList = await prisma.wiki.findMany();
+
     return (
         <main>
             <p>위키 조회 페이지</p>
+            {wikiList.map(({ title, content, id }) => {
+                return (
+                    <div key={id}>
+                        <p>{title}</p>
+                        <p>{content}</p>
+                    </div>
+                );
+            })}
         </main>
     );
 }
